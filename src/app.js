e4d328d5f7b14889a79d82e70a44d8e25e592f84
Hasibul <skhasibulhossain16@gmail.com>
2026-08-04 06:58:06 +0530

    change some code

--- src/app.js
+++ src/app.js
@@ -17,9 +17,10 @@ app.use(express.json());
 app.use(
   cors({
     origin: "*",
-    credentials: true,
   }),
 );
+
+app.options("*", cors());
 
 // app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
