d5334320a2c4196e0c6ecf5d51e2457755b19b2d
Hasibul <skhasibulhossain16@gmail.com>
2026-08-04 07:05:54 +0530

    change some code

--- src/app.js
+++ src/app.js
@@ -17,6 +17,8 @@ app.use(express.json());
 app.use(
   cors({
     origin: "*",
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   }),
 );
 
