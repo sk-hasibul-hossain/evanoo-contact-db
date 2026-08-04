ff6d8576b85f6bb7bc585dae2f497338b4a968a7
Hasibul <skhasibulhossain16@gmail.com>
2026-08-04 07:51:34 +0530

    change some code

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "https://cors-api-5pqw.vercel.app",
+    origin: "https://evanoo-contact-db.vercel.app",
   }),
 );
 
